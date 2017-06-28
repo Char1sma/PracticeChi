32752bc2ae905668efe79eeece72b42a41711a7c
alex <alexxkareev@gmail.com>
2017-06-28 22:46:41 +0300

    Newer Version

--- OpenCV_test/main.cpp
+++ OpenCV_test/main.cpp
@@ -34,7 +34,7 @@ bool Change(Mat &base, Mat &img, Vect V)
 			img.at<Vec3b>(i, j)[2] = 0;
 		}
 
-	
+
 
 	double coeff;
 	Straight *S1[4];
@@ -51,17 +51,24 @@ bool Change(Mat &base, Mat &img, Vect V)
 		for (int j = 0; j < Changed[i].size(); j++)
 			Changed[i][j] = false;
 	}
+	
+	img.at<Vec3b>(V.epoint.x, V.epoint.y)[0] = img.at<Vec3b>(V.spoint.x, V.spoint.y)[0];
+	img.at<Vec3b>(V.epoint.x, V.epoint.y)[1] = img.at<Vec3b>(V.spoint.x, V.spoint.y)[1];
+	img.at<Vec3b>(V.epoint.x, V.epoint.y)[2] = img.at<Vec3b>(V.spoint.x, V.spoint.y)[2];
+
+	Changed[V.epoint.x][V.epoint.y] = true;
+
 	for (int i = 0; i < img.rows; i++)
 		for (int j = 0; j < img.cols; j++)
 		{
-			
-			if (i != V.spoint.x||j != V.spoint.y)
+
+			if (i != V.spoint.x || j != V.spoint.y)
 			{
-				
+
 				Straight S = Straight(V.spoint, Point(i, j));
-				
+
 				int k = 0;
-				bool stop = false;			
+				bool stop = false;
 				while ((k < 4) && !stop)
 				{
 					Point temp;
@@ -74,28 +81,59 @@ bool Change(Mat &base, Mat &img, Vect V)
 						}
 					k++;
 				}
-				img.at<Vec3b>(i, j)[2] = 255;
-				cout << coeff << endl;
-				//cout << func(coeff) << endl;
 				coeff = hyperb(coeff);
 				Vect res = V * coeff;
 				res.Move(cv::Point(i, j));
 				B = base.at<Vec3b>(i, j)[0];
 				G = base.at<Vec3b>(i, j)[1];
 				R = base.at<Vec3b>(i, j)[2];
-				/*if (res.epoint.x <= img.rows&&res.epoint.y <= img.cols)
+				if (res.epoint.x < img.rows && res.epoint.y < img.cols)
 				{
-					img.at<Vec3b>(res.epoint.x, res.epoint.y)[0] = 255;
-					img.at<Vec3b>(res.epoint.x, res.epoint.y)[1] = 0;
-					img.at<Vec3b>(res.epoint.x, res.epoint.y)[2] = 0;
-				}*/
-				//res.ShowBase();
+					img.at<Vec3b>(res.epoint.x, res.epoint.y)[0] = B;
+					img.at<Vec3b>(res.epoint.x, res.epoint.y)[1] = G;
+					img.at<Vec3b>(res.epoint.x, res.epoint.y)[2] = R;
+					Changed[res.epoint.x][res.epoint.y] = true;
+				}
 			}
 		}
+	//вот тут начинается заполнение битых точек
+	int counter;
+	for (int i = 0; i < img.rows; i++)
+		for (int j = 0; j < img.cols; j++)
+		{
+			if (!Changed[i][j])
+			{
+				counter = 0;
+				int step = 1;
+				int B = 0;
+				int G = 0;
+				int R = 0;
+				while (counter < 1)
+				{
+					for (int k = i - step; k <= i + step; k++)
+						for (int l = j - step; l <= j + step; l++)
+						{
+							if (k > -1 && l > -1 && k < img.rows && l < img.cols && Changed[k][l])
+							{
+								counter++;
+								B += img.at<Vec3b>(k, l)[0];
+								G += img.at<Vec3b>(k, l)[1];
+								R += img.at<Vec3b>(k, l)[2];
+								
+							}
+						}
 
-	
-
-	
+					step++;
+				}
+				B /= counter;
+				G /= counter;
+				R /= counter;
+				img.at<Vec3b>(i, j)[0] = B;
+				img.at<Vec3b>(i, j)[1] = G;
+				img.at<Vec3b>(i, j)[2] = R;
+			}
+		}
+	//а тут заканчивается
 	return true;
 }
 
@@ -105,18 +143,13 @@ IplImage* dst = 0;
 int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "rus");
-
 	char* filename = "test.jpg";
-
 	image = cvLoadImage(filename, 1);
 	dst = cvCloneImage(image);
-
 	printf("[i] image: %s\n", filename);
 	assert(image != 0);
-
-	cvNamedWindow("original", CV_WINDOW_AUTOSIZE);
-	cvNamedWindow("new", CV_WINDOW_AUTOSIZE);
-
+	namedWindow("original", CV_WINDOW_AUTOSIZE);
+	namedWindow("new", CV_WINDOW_AUTOSIZE);
 
 	cvShowImage("original", image);
 
@@ -134,7 +167,7 @@ int main(int argc, char* argv[])
 	printf("[i] height:      %d pixels\n", image->height);
 	printf("[i] image size:  %d bytes\n", image->imageSize);
 	printf("[i] width step:  %d bytes\n", image->widthStep);*/
-	cvWaitKey(0);
+	waitKey(0);
 	cvReleaseImage(&image);
 	cvReleaseImage(&dst);
 	cvDestroyWindow("original");
